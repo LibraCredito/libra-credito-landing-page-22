3e7a18721b99932762dfb9b7a9da525e73027bd1
cwolfbr <raul.pedro.mv@gmail.com>
2025-07-28 16:44:07 -0300

    revert: return to commit 9af3c64

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 
 import type { Config } from "tailwindcss";
-import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
         content: [
@@ -153,5 +152,5 @@ export default {
 			}
 		}
 	},
-       plugins: [tailwindcssAnimate],
+	plugins: [require("tailwindcss-animate")],
 } satisfies Config;
