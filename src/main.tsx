115c3641e2d239b609394bc644f3b5a0469135f1
cwolfbr <raul.pedro.mv@gmail.com>
2025-07-28 14:00:19 -0300

    fix: load main stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx'
+import './index.css';
 import './styles/overflow-fix.css';
 
 // Renderização direta com createRoot para LCP otimizado
