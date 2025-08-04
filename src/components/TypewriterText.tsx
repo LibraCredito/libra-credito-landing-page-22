5d3e89d871de73e2262fc6ce6e8b27a14b8bef11
LibraCredito <devs@libracredito.com.br>
2025-08-04 09:40:32 -0300

    refactor: load typed.js dynamically

--- src/components/TypewriterText.tsx
+++ src/components/TypewriterText.tsx
@@ -1,19 +1,23 @@
 import { useEffect, useRef } from 'react';
-import Typed from 'typed.js';
 
 export default function TypewriterText({ strings }: { strings: string[] }) {
   const el = useRef<HTMLSpanElement>(null);
 
   useEffect(() => {
-    const typed = new Typed(el.current!, {
-      strings,
-      typeSpeed: 50,
-      backSpeed: 30,
-      backDelay: 1500,
-      loop: true,
+    let typed: any;
+
+    import('typed.js').then(({ default: Typed }) => {
+      typed = new Typed(el.current!, {
+        strings,
+        typeSpeed: 50,
+        backSpeed: 30,
+        backDelay: 1500,
+        loop: true,
+      });
     });
-    return () => typed.destroy();
+
+    return () => typed?.destroy();
   }, [strings]);
 
-  return <span ref={el} />;
+  return <span ref={el}>{strings[0]}</span>;
 }
